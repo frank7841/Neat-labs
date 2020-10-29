3c2ca9ab09d8bb2f24ea187515d035c58f55f0dc
frank7841 <franklinewax@gmail.com>
2020-10-29 20:42:01 +0300

    initial commit at navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Nav} from './Navbar.elements'
+const Navbar = () => {
+    return (
+        <>
+          <Nav>
+            <NavabaeContainer>
+              
+            </NavabaeContainer>
+          </Nav>  
+        </>
+    )
+}
+
+export default Navbar
